edcd4804984c8a60443b3b1ef0cce381b00200c4
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-02-04 22:26:34 +0100

    delete test folder

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,18 +7,23 @@ import 'dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map((Category category) => CategoryItem(
-                title: category.title,
-                color: category.color,
-              ))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((Category category) => CategoryItem(
+                  title: category.title,
+                  color: category.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
