8e87824ac58ded0ddee871a634e4f41e1881ba47
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-02-08 22:26:54 +0100

    display the other data in MealItem

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -17,6 +17,33 @@ class MealItem extends StatelessWidget {
   final Complexity complexity;
   final Affordability affordability;
 
+
+  String get complexityText {
+    switch(complexity) {
+      case Complexity.Simple:
+        return 'Simple';break;
+      case Complexity.Challenging:
+        return 'Hard';break;
+      case Complexity.Hard:
+        return 'Challenging';break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch(affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';break;
+      case Affordability.Pricey:
+        return 'Pricey';break;
+      case Affordability.Luxurious:
+        return 'Luxurious';break;
+      default:
+        return 'Unknown';
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -44,8 +71,39 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
-                )
+                ),
+                Positioned(
+                    bottom: 20,
+                    right: 10,
+                    child: Container(
+                      width: 300,
+                      color: Colors.black54,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                        ),
+                        softWrap: true,
+                        overflow: TextOverflow.fade,
+                      ),
+                    ))
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(children: [Icon(Icons.schedule), SizedBox(width: 6,), Text('$duration min')],),
+                  Row(children: [Icon(Icons.work), SizedBox(width: 6,), Text(complexityText)],),
+                  Row(children: [Icon(Icons.attach_money), SizedBox(width: 6,), Text(affordabilityText)],),
+                  // Row(children: [Icon(), Text()],),
+                  // Row(children: [Icon(), Text()],),
+                ],
+              ),
             )
           ],
         ),
