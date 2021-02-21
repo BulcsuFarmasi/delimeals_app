c3086a62f80f220e7db441d1c672e3fa79d5cc17
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-02-21 23:33:35 +0100

    use pop and get back data from it

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -4,18 +4,41 @@ import '../widgets/meal_item.dart';
 import '../dummy_data.dart';
 import '../models/meal.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+
+  static const String routeName = '/category-meals';
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
   // CategoryMealsScreen({this.categoryId, this.categoryTitle});
   //
-  // final String categoryId;
-  // final String categoryTitle;
+
+  String categoryTitle;
+  List<Meal> categoryMeals;
+  bool _loadedInitData = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_loadedInitData) {
+      final Map<String, String> routeArguments = ModalRoute.of(context).settings.arguments as Map<String, String>;
+      final categoryId = routeArguments['id'];
+      categoryTitle = routeArguments['title'];
+      categoryMeals = DUMMY_MEALS.where((Meal meal) => meal.categories.contains(categoryId)).toList();
+      _loadedInitData = true;
+    }
+  }
+
+  void _removeItem(String mealId) {
+    setState(() {
+      categoryMeals.removeWhere((Meal meal) => meal.id == mealId);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final Map<String, String> routeArguments = ModalRoute.of(context).settings.arguments as Map<String, String>;
-    final String categoryId = routeArguments['id'];
-    final String categoryTitle = routeArguments['title'];
-    final List<Meal> categoryMeals = DUMMY_MEALS.where((Meal meal) => meal.categories.contains(categoryId)).toList();
     return Scaffold(
       appBar: AppBar(title: Text(categoryTitle)),
       body: ListView.builder(
@@ -27,12 +50,11 @@ class CategoryMealsScreen extends StatelessWidget {
             duration: categoryMeals[index].duration,
             complexity: categoryMeals[index].complexity,
             affordability: categoryMeals[index].affordability,
+            removeItem: _removeItem,
           );
         },
         itemCount: categoryMeals.length,
       ),
     );
   }
-
-  static const String routeName = '/category-meals';
 }
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -34,56 +34,64 @@ class MealDetailScreen extends StatelessWidget {
     final String mealId = ModalRoute.of(context).settings.arguments as String;
     final Meal meal = DUMMY_MEALS.firstWhere((Meal meal) => meal.id == mealId);
     final ThemeData theme = Theme.of(context);
+    final NavigatorState navigator = Navigator.of(context);
     return Scaffold(
-        appBar: AppBar(
-          title: Text('${meal.title}'),
+      appBar: AppBar(
+        title: Text('${meal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            buildSectionTitle('Ingredients', theme),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    color: theme.accentColor,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      child: Text(meal.ingredients[index]),
+                    ),
+                  );
+                },
+                itemCount: meal.ingredients.length,
+              ),
+            ),
+            buildSectionTitle('Steps', theme),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('#${index + 1}'),
+                      ),
+                      title: Text(meal.steps[index]),
+                    ),
+                    Divider(),
+                  ]);
+                },
+                itemCount: meal.steps.length,
+              ),
+            )
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Image.network(
-                  meal.imageUrl,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              buildSectionTitle('Ingredients', theme),
-              buildContainer(
-                ListView.builder(
-                  itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                      color: theme.accentColor,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        child: Text(meal.ingredients[index]),
-                      ),
-                    );
-                  },
-                  itemCount: meal.ingredients.length,
-                ),
-              ),
-              buildSectionTitle('Steps', theme),
-              buildContainer(
-                ListView.builder(
-                  itemBuilder: (BuildContext context, int index) {
-                    return Column(children: [
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Text('#${index + 1}'),
-                        ),
-                        title: Text(meal.steps[index]),
-                      ),
-                      Divider(),
-                    ]);
-                  },
-                  itemCount: meal.steps.length,
-                ),
-              )
-            ],
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.delete),
+        onPressed: () {
+          navigator.pop(mealId);
+        },
+      ),
+    );
   }
 
   static const String routeName = '/meal-detail';
